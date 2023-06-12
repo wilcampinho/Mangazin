533a4f8b8e9a5004628e4f002165acf85454dead
Wilson Campinho <43688711+wilcampinho@users.noreply.github.com>
2023-06-12 17:25:39 -0300

    initial commit

---
+++ test/InitialTest.test.ts
@@ -0,0 +1,38 @@
+describe("Feature: X", () =>
+{
+  it("Y", () =>
+  {
+    // Given
+    // let sut = new PLACEHOLDER
+
+    // When
+    // ACTION
+
+    // Then
+    // expect().toBe();
+  });
+
+  it("W", () =>
+  {
+    // Given
+    // let sut = new PLACEHOLDER
+
+    // When
+    // ACTION
+
+    // Then
+    // expect().toBe();
+  });
+
+  it("Z", () =>
+  {
+    // Given
+    // let sut = new PLACEHOLDER
+
+    // When
+    // ACTION
+
+    // Then
+    // expect().toBe();
+  });
+});
