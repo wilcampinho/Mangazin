6257195d3d57272d330cd0fe5dd734da1854125a
Wilson Campinho <43688711+wilcampinho@users.noreply.github.com>
2023-07-03 18:32:46 -0300

    Create index.js

---
+++ src/routes/User/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const handlers = require('./handlers');
+
+router.post('/user/add/', handlers.addUser);
+
+
+module.exports = router;
